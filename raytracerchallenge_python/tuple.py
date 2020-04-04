7a801dcc9b98ba4bf69d88196f9c7b5f6542e34a
Fumitaka Tokumitsu <toku345@gmail.com>
2020-04-04 15:39:40 +0900

    Add 2 Scenarios & Implementations (pp. 4 latter)

--- raytracerchallenge_python/tuple.py
+++ raytracerchallenge_python/tuple.py
@@ -10,3 +10,23 @@ class Tuple:
 
     def is_vector(self):
         return not self.is_point()
+
+    def __eq__(self, other):
+        """Overrides the default implementation"""
+        if not isinstance(other, Tuple):
+            return False
+
+        return (
+            self.x == other.x
+            and self.y == other.y
+            and self.z == other.z
+            and self.w == other.w
+        )
+
+
+def point(x, y, z):
+    return Tuple(x, y, z, 1.0)
+
+
+def vector(x, y, z):
+    return Tuple(x, y, z, 0.0)
--- tests/test_tuples.py
+++ tests/test_tuples.py
@@ -1,4 +1,4 @@
-from raytracerchallenge_python.tuple import Tuple
+from raytracerchallenge_python.tuple import Tuple, point, vector
 
 
 def test_a_tuple_with_w_equal_1_is_a_point():
@@ -23,3 +23,27 @@ def test_a_tuple_with_w_equal_0_is_a_vector():
     assert a.w == 0.0
     assert not a.is_point()
     assert a.is_vector()
+
+
+def test_equivalence_tuple_objects():
+    a = Tuple(4.3, -4.2, 3.1, 1.0)
+    assert a == Tuple(4.3, -4.2, 3.1, 1.0)
+    assert a != Tuple(9.9, -4.2, 3.1, 1.0)
+    assert a != Tuple(4.3,  9.9, 3.1, 1.0)
+    assert a != Tuple(4.3, -4.2, 9.9, 1.0)
+    assert a != Tuple(4.3, -4.2, 3.1, 0.0)
+    assert a != (4.3, -4.2, 3.1, 1.0)
+
+
+def test_func_point_creates_tuples_with_w_equal_1():
+    # Given
+    p = point(4, -4, 3)
+    # Then
+    assert p == Tuple(4, -4, 3, 1)
+
+
+def test_func_vector_creates_tuples_with_w_equal_0():
+    # Given
+    p = vector(4, -4, 3)
+    # Then
+    assert p == Tuple(4, -4, 3, 0)
