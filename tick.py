99d6a1c11819fc90bb7d686331c53b0f8fd768ee
Fumitaka Tokumitsu <toku345@gmail.com>
2020-04-09 11:34:37 +0900

    Chap1: Putting It Together

---
+++ tick.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+from dataclasses import dataclass
+from raytracerchallenge_python.tuple import Point, Vector
+
+
+@dataclass
+class Projectile:
+    position: Point
+    velocity: Vector
+
+
+@dataclass
+class Environment:
+    gravity: Vector
+    wind: Vector
+
+
+def tick(env, proj):
+    position = proj.position + proj.velocity
+    velocity = proj.velocity + env.gravity + env.wind
+    return Projectile(position, velocity)
+
+
+if __name__ == '__main__':
+    # projectile starts one unit above the origin.
+    # velocity is normalized to 1 unit/tick.
+    p = Projectile(Point(0, 1, 0), Vector(1, 1, 0).normalize())
+
+    # gravity -0.1 unit/tick, and wind is -0.01 unit/tick
+    e = Environment(Vector(0, -0.1, 0), Vector(-0.01, 0, 0))
+
+    while p.position.y > 0:
+        p = tick(e, p)
+        print('[Projectile position] x: ', p.position.x,
+              ' y: ', p.position.y,
+              ' z: ', p.position.z)
