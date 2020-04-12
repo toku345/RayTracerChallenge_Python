b78ef2ae22e31f7ad87b6560210716b41aab758c
Fumitaka Tokumitsu <toku345@gmail.com>
2020-04-12 16:07:46 +0900

    Chap2: Putting It Together
    Usage:
      $ ./tick.py > projectile.pppm

--- tick.py
+++ tick.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 from dataclasses import dataclass
-from raytracerchallenge_python.tuple import Point, Vector
+from math import floor
+
+from raytracerchallenge_python.tuple import Point, Vector, Color
+from raytracerchallenge_python.canvas import Canvas
 
 
 @dataclass
@@ -22,15 +25,25 @@ def tick(env, proj):
 
 
 if __name__ == "__main__":
-    # projectile starts one unit above the origin.
-    # velocity is normalized to 1 unit/tick.
-    p = Projectile(Point(0, 1, 0), Vector(1, 1, 0).normalize())
+    start = Point(0, 1, 0)
+    velocity = Vector(1, 1.8, 0).normalize() * 11.25
+    p = Projectile(start, velocity)
 
-    # gravity -0.1 unit/tick, and wind is -0.01 unit/tick
-    e = Environment(Vector(0, -0.1, 0), Vector(-0.01, 0, 0))
+    gravity = Vector(0, -0.1, 0)
+    wind = Vector(-0.01, 0, 0)
+    e = Environment(gravity, wind)
+
+    width = 900
+    height = 550
+    c = Canvas(width, height)
+
+    color = Color(1, 0, 0)
 
     while p.position.y > 0:
         p = tick(e, p)
-        print("[Projectile position] x: ", p.position.x,
-              " y: ", p.position.y,
-              " z: ", p.position.z)
+        x = floor(p.position.x)
+        y = height - floor(p.position.y)
+        if 0 <= x < width and 0 <= y < height:
+            c.write_pixel(x, y, color)
+
+    print(c.to_ppm())
