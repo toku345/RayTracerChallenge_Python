1a24b54965d05f132690d67a8ded07a9712a08a0
Fumitaka Tokumitsu <toku345@gmail.com>
2020-04-15 15:34:13 +0900

    Add Scenario & Implementation (pp. 46-47 `Scaling`)

--- raytracerchallenge_python/transformations.py
+++ raytracerchallenge_python/transformations.py
@@ -6,3 +6,10 @@ def translation(x, y, z):
                   0, 1, 0, y,
                   0, 0, 1, z,
                   0, 0, 0, 1)
+
+
+def scaling(x, y, z):
+    return Matrix(x, 0, 0, 0,
+                  0, y, 0, 0,
+                  0, 0, z, 0,
+                  0, 0, 0, 1)
--- tests/test_transformations.py
+++ tests/test_transformations.py
@@ -1,4 +1,4 @@
-from raytracerchallenge_python.transformations import translation
+from raytracerchallenge_python.transformations import translation, scaling
 from raytracerchallenge_python.tuple import Point, Vector
 
 
@@ -25,3 +25,36 @@ def test_translation_does_not_affect_vectors():
     v = Vector(-3, 4, 5)
     # Then
     assert transform * v == v
+
+
+def test_scaling_matrix_applied_to_a_point():
+    # Given
+    trasform = scaling(2, 3, 4)
+    p = Point(-4, 6, 8)
+    # Then
+    assert trasform * p == Point(-8, 18, 32)
+
+
+def test_a_scaling_matrix_applied_to_a_vector():
+    # Given
+    tranform = scaling(2, 3, 4)
+    v = Vector(-4, 6, 8)
+    # Then
+    assert tranform * v == Vector(-8, 18, 32)
+
+
+def test_multiplying_by_the_inverse_of_a_scaling_matrix():
+    # Given
+    transform = scaling(2, 3, 4)
+    inv = transform.inverse()
+    v = Vector(-4, 6, 8)
+    # Then
+    assert inv * v == Vector(-2, 2, 2)
+
+
+def test_reflection_is_scaling_by_a_negative_value():
+    # Given
+    transform = scaling(-1, 1, 1)
+    p = Point(2, 3, 4)
+    # Then
+    assert transform * p == Point(-2, 3, 4)
