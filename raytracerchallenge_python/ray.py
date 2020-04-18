b5409529deb29161f0e363f1d89b1bf0a9e19e84
Fumitaka Tokumitsu <toku345@gmail.com>
2020-04-18 15:54:16 +0900

    Add Scenario & Implementation (pp. 66-69 `Transforming Rays and Spheres` ray's scenario & implementation)

--- raytracerchallenge_python/ray.py
+++ raytracerchallenge_python/ray.py
@@ -5,3 +5,6 @@ class Ray:
 
     def position(self, t):
         return self.origin + self.direction * t
+
+    def transform(self, matrix):
+        return Ray(matrix * self.origin, matrix * self.direction)
--- tests/test_rays.py
+++ tests/test_rays.py
@@ -1,4 +1,5 @@
 from raytracerchallenge_python.ray import Ray
+from raytracerchallenge_python.transformations import translation, scaling
 from raytracerchallenge_python.tuple import Point, Vector
 
 
@@ -21,3 +22,25 @@ def test_computing_a_point_from_a_distance():
     assert r.position(1) == Point(3, 3, 4)
     assert r.position(-1) == Point(1, 3, 4)
     assert r.position(2.5) == Point(4.5, 3, 4)
+
+
+def test_translating_a_ray():
+    # Given
+    r = Ray(Point(1, 2, 3), Vector(0, 1, 0))
+    m = translation(3, 4, 5)
+    # When
+    r2 = r.transform(m)
+    # Then
+    assert r2.origin == Point(4, 6, 8)
+    assert r2.direction == Vector(0, 1, 0)
+
+
+def test_scaling_a_ray():
+    # Given
+    r = Ray(Point(1, 2, 3), Vector(0, 1, 0))
+    m = scaling(2, 3, 4)
+    # When
+    r2 = r.transform(m)
+    # Then
+    assert r2.origin == Point(2, 6, 12)
+    assert r2.direction == Vector(0, 3, 0)
