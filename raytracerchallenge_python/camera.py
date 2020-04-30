423ad56ef4ba8239d9b6fb9239946bceebeed9d9
Fumitaka Tokumitsu <toku345@gmail.com>
2020-05-01 07:00:46 +0900

    Add Scenario & Implementation (pp. 101-103 `Implementing a Camera` calc pixel_size)

--- raytracerchallenge_python/camera.py
+++ raytracerchallenge_python/camera.py
@@ -1,5 +1,7 @@
 from raytracerchallenge_python.matrix import identity_matrix
 
+from math import tan
+
 
 class Camera:
     def __init__(self, hsize, vsize, field_of_view):
@@ -7,3 +9,16 @@ class Camera:
         self.vsize = vsize
         self.field_of_view = field_of_view
         self.transform = identity_matrix()
+
+        self._calc_pixel_size()
+
+    def _calc_pixel_size(self):
+        half_view = tan(self.field_of_view / 2)
+        aspect = self.hsize / self.vsize
+        if aspect >= 1:
+            self.half_width = half_view
+            self.half_height = half_view / aspect
+        else:
+            self.half_width = half_view * aspect
+            self.half_height = half_view
+        self.pixel_size = (self.half_width * 2) / self.hsize
--- tests/test_camera.py
+++ tests/test_camera.py
@@ -1,6 +1,8 @@
 from raytracerchallenge_python.camera import Camera
 from raytracerchallenge_python.matrix import identity_matrix
 
+from raytracerchallenge_python.helpers import equal
+
 from math import pi
 
 
@@ -16,3 +18,17 @@ def test_constructing_a_camera():
     assert c.vsize == 120
     assert c.field_of_view == pi / 2
     assert c.transform == identity_matrix()
+
+
+def test_the_pixel_size_for_a_horizontal_canvas():
+    # Given
+    c = Camera(200, 125, pi / 2)
+    # Then
+    assert equal(c.pixel_size, 0.01)
+
+
+def test_the_pixel_size_for_a_vertical_canvas():
+    # Given
+    c = Camera(125, 200, pi / 2)
+    # Then
+    assert equal(c.pixel_size, 0.01)
