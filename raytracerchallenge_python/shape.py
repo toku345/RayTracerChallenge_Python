4d7be380b6d93b8de8639d9c9971121bf4866c9c
Fumitaka Tokumitsu <toku345@gmail.com>
2020-05-05 16:48:56 +0900

    Add Scenario & Implementation (pp. 119 `Refactoring Shaptes` Add default material -> Shape))

--- raytracerchallenge_python/shape.py
+++ raytracerchallenge_python/shape.py
@@ -1,8 +1,10 @@
 from abc import ABCMeta
 
 from raytracerchallenge_python.matrix import identity_matrix
+from raytracerchallenge_python.material import Material
 
 
 class Shape(metaclass=ABCMeta):
     def __init__(self):
         self.transform = identity_matrix()
+        self.material = Material()
--- tests/test_shapes.py
+++ tests/test_shapes.py
@@ -1,6 +1,7 @@
 from raytracerchallenge_python.matrix import identity_matrix
 from raytracerchallenge_python.transformations import translation
 from raytracerchallenge_python.shape import Shape
+from raytracerchallenge_python.material import Material
 
 
 class MockShape(Shape):
@@ -21,3 +22,23 @@ def test_assigning_a_transformation():
     s.transform = translation(2, 3, 4)
     # Then
     assert s.transform == translation(2, 3, 4)
+
+
+def test_the_default_material():
+    # Given
+    s = MockShape()
+    # When
+    m = s.material
+    # Then
+    assert m == Material()
+
+
+def test_assigning_a_material():
+    # Given
+    s = MockShape()
+    m = Material()
+    m.ambient = 1
+    # When
+    s.material = m
+    # Then
+    assert s.material == m
