153d3476acdc5a529cb88812c7d2dd7b07937701
Fumitaka Tokumitsu <toku345@gmail.com>
2020-05-23 14:23:00 +0900

    Chap12: Putting It Together (1 cube)

--- renderer.py
+++ renderer.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python3
-from raytracerchallenge_python.sphere import Sphere, glass_sphere
-from raytracerchallenge_python.plane import Plane
-from raytracerchallenge_python.material import Material
-from raytracerchallenge_python.tuple import Color, Point, Vector
-from raytracerchallenge_python.camera import Camera
-from raytracerchallenge_python.world import World
-from raytracerchallenge_python.point_light import PointLight
+from math import pi
+from raytracerchallenge_python.gradient_pattern import GradientPattern
 from raytracerchallenge_python.transformations import (
     scaling, view_transform, translation, rotation_y, rotation_x)
+from raytracerchallenge_python.point_light import PointLight
+from raytracerchallenge_python.world import World
+from raytracerchallenge_python.camera import Camera
+from raytracerchallenge_python.tuple import Color, Point, Vector
+from raytracerchallenge_python.material import Material
+from raytracerchallenge_python.sphere import Sphere, glass_sphere
+from raytracerchallenge_python.plane import Plane
+from raytracerchallenge_python.cube import Cube
 # from raytracerchallenge_python.stripe_pattern import StripePattern
 # from raytracerchallenge_python.checkers_pattern import CheckersPattern
-from raytracerchallenge_python.gradient_pattern import GradientPattern
 # from raytracerchallenge_python.ring_pattern import RingPattern
 
-from math import pi
 
 if __name__ == "__main__":
     # pattern = StripePattern(Color(1, 0, 0), Color(1, 1, 1))
@@ -68,8 +69,13 @@ if __name__ == "__main__":
     left.material.diffuse = 0.7
     left.material.specular = 0.3
 
+    cube = Cube()
+    cube.transform = translation(
+        0, 0, 1) * rotation_y(pi / 4) * scaling(0.5, 0.5, 0.5)
+
     world = World()
-    world.objects = [floor, backdrop, backdrop2, middle, right, left]
+    # world.objects = [floor, backdrop, backdrop2, middle, right, left]
+    world.objects = [cube]
     world.light = PointLight(Point(-10, 10, -10), Color(1, 1, 1))
 
     camera = Camera(100, 50, pi / 3)
