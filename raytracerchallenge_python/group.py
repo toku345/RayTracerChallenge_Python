f747786b2886b25bba72fd0983d93bd8da9b9f70
Fumitaka Tokumitsu <toku345@gmail.com>
2020-06-01 06:06:17 +0900

    Add Scenario & Implementation (pp. 195 `Implementing Groups: Test #3`)

--- raytracerchallenge_python/group.py
+++ raytracerchallenge_python/group.py
@@ -8,3 +8,10 @@ class Group:
 
     def __len__(self):
         return len(self.children)
+
+    def __iter__(self):
+        return iter(self.children)
+
+    def add_child(self, shape):
+        self.children.append(shape)
+        shape.parent = self
--- tests/test_groups.py
+++ tests/test_groups.py
@@ -1,5 +1,6 @@
 from raytracerchallenge_python.group import Group
 from raytracerchallenge_python.matrix import identity_matrix
+from raytracerchallenge_python.shape import Shape
 
 
 def test_creating_a_new_group():
@@ -8,3 +9,23 @@ def test_creating_a_new_group():
     # Then
     assert g.transform == identity_matrix()
     assert len(g) == 0
+
+
+class MockShape(Shape):
+    def local_intersect(self, ray):
+        pass
+
+    def local_normal_at(self, p):
+        pass
+
+
+def test_adding_a_child_to_a_group():
+    # Given
+    g = Group()
+    s = MockShape()
+    # When
+    g.add_child(s)
+    # Then
+    assert len(g) > 0
+    assert s in g
+    assert s.parent == g
